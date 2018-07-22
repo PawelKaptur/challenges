d88967c324f30a314569049cab0d2b3f246eb126
Pawel Kaptur <pawel.kaptur@protonmail.com>
2018-07-22 11:56:13 +0200

    init

---
+++ src/main/java/com/capgemini/challenges/ChallengesApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengesApplication.class, args);
+	}
+}
