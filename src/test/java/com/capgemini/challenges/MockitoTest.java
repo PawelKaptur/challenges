1c87001d39f308104d418c24e2820cd329bcb62b
Pawel Kaptur <pawel.kaptur@protonmail.com>
2018-07-26 20:54:15 +0200

    test with mockito

--- src/test/java/com/capgemini/challenges/MockitoTest.java
+++ src/test/java/com/capgemini/challenges/MockitoTest.java
@@ -1,7 +1,60 @@
 package com.capgemini.challenges;
 
+import com.capgemini.challenges.abilitytime.service.AbilityTimeService;
+import com.capgemini.challenges.player.dao.PlayerDAO;
+import com.capgemini.challenges.player.dto.PlayerDTO;
+import com.capgemini.challenges.player.mapper.PlayerMapper;
+import com.capgemini.challenges.player.service.PlayerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MockitoTest {
 
+
+    @Mock
+    private PlayerDAO playerDAO;
+
+    @Mock
+    private PlayerMapper playerMapper;
+
+    @Mock
+    private AbilityTimeService abilityTimeService;
+
+    @InjectMocks
+    private PlayerService playerService;
+
+    @Before
+    public void setup() {
+        PlayerDTO player = new PlayerDTO();
+        List<PlayerDTO> players = new ArrayList<>();
+        players.add(player);
+        players.add(player);
+
+        when(playerService.searchPlayerByUsername(Mockito.anyString())).thenReturn(players);
+    }
+
+    @Test
+    public void shouldAssertEqualsBecauseMockedSizeTwo() {
+        //given
+
+        //when
+        List<PlayerDTO> newPlayers = playerService.searchPlayerByUsername("Janusz");
+
+        //then
+        assertThat(newPlayers.size()).isEqualTo(2);
+    }
 }
